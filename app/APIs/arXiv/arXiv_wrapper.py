43f87002bc18a6239533e3d273c0f434c2de9cc6
Sulaiman Mulla <sulaiman_1@tamu.edu>
2024-11-07 00:17:05 -0600

    upgraded arxiv API wrapper

--- app/APIs/arXiv/arXiv_wrapper.py
+++ app/APIs/arXiv/arXiv_wrapper.py
@@ -1,11 +1,12 @@
 import arxiv
+from arxiv import UnexpectedEmptyPageError
 
 class api_handler:
     
     def __init__(self):
         self.client = arxiv.Client()
 
-    def query(self, query, max_results):
+    def query(self, query, max_results=None):
         search = arxiv.Search(
             query=query,
             sort_by=arxiv.SortCriterion.Relevance
@@ -13,10 +14,15 @@ class api_handler:
         # Generator function to yield results with a DOI
         def results_with_doi():
             count = 0
-            for result in self.client.results(search):
-                if result.doi:
-                    yield result
-                    count += 1
-                    if count >= max_results:
-                        break
+            try:
+                for result in self.client.results(search):
+                    if result.doi:
+                        yield result
+                        count += 1
+                        if max_results is not None and count >= max_results:
+                            break
+            except UnexpectedEmptyPageError as e:
+                print(f"Unexpected empty page encountered: {e}")
+            except Exception as e:
+                print(f"An error occurred: {e}")
         return results_with_doi()
